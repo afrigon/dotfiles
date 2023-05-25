62bb10358d63dd62f51775c94fc0c927134f34dc
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2023-05-25 00:54:30 -0400

    a few options

---
+++ config/nvim/lua/core/options.lua
@@ -0,0 +1,50 @@
+-- enable system clipbaord
+vim.opt.clipboard = "unnamedplus"
+
+-- enable line numbers
+vim.opt.nu = true
+
+-- enable relative line numbers
+vim.opt.rnu = true
+
+-- disable line wrapping
+vim.opt.wrap = false
+
+-- enable auto identation, copy ident when creating a new line
+vim.opt.ai = true
+
+-- enable smart identation, smartly indent based on scope when creating a new line
+vim.opt.si = true
+
+-- set tab stop, number of spaces a <Tab> counts for
+vim.opt.ts = 4
+
+-- set soft tab stop, used when inserting a <Tab>
+vim.opt.sts = 4
+
+-- set shift width, used for each step of indent
+vim.opt.sw = 4
+
+-- enable expand tab, turns <Tab> into <Space> when inserting
+vim.opt.et = true
+
+-- enable terminal terminal gui colors
+vim.opt.tgc = true
+
+-- set the scroll offset, the minimum number of screen line to keep above and below
+vim.opt.so = 8
+
+-- enable persistent undo
+vim.opt.udf = true
+
+-- set the line wimber width minimum
+vim.opt.nuw = 2
+
+-- set the file encoding
+vim.opt.encoding = "utf-8"
+
+-- enable search highlighting
+vim.opt.hls = true
+
+-- enable incremental searching
+vim.opt.is = true
