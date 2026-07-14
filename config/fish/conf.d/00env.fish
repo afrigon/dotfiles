114d1eabc851eefaa2b7b659ef660d597fad4849
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2026-07-14 19:18:15 -0400

    arch update

--- config/fish/conf.d/00env.fish
+++ config/fish/conf.d/00env.fish
@@ -1,9 +1,13 @@
-set -x CLAUDE_CONFIG_DIR ~/.config/claude
+set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+
 set -x EDITOR nvim
 set -x GIT_EDITOR $EDITOR
 set -x SUDO_EDITOR $EDITOR
 set -x VISUAL $EDITOR
 set -x BROWSER firefox
+
+set -x CLAUDE_CONFIG_DIR ~/.config/claude
 
 switch (uname)
 case Linux
