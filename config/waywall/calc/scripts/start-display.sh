b63f7beea09e08a82e9dc9de01e306a5a6e860dd
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2026-08-21 01:00:31 -0400

    f3 line

--- config/waywall/calc/scripts/start-display.sh
+++ config/waywall/calc/scripts/start-display.sh
@@ -6,6 +6,7 @@ dir="$(cd "$(dirname "$0")/.." && pwd)"
     sleep 1
     eww --config "$dir" open calc 2>/dev/null
     eww --config "$dir" open boat 2>/dev/null
+    eww --config "$dir" open f3line 2>/dev/null
 ) &
 
 exec eww --config "$dir" daemon --no-daemonize
