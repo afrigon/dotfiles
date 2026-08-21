ad55db61da265f1a579dfe14fa6f3e79646e306e
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2026-08-21 01:39:11 -0400

    clean up

--- config/waywall/calc/scripts/start-display.sh
+++ config/waywall/calc/scripts/start-display.sh
@@ -4,8 +4,6 @@ dir="$(cd "$(dirname "$0")/.." && pwd)"
 
 (
     sleep 1
-    eww --config "$dir" open calc 2>/dev/null
-    eww --config "$dir" open boat 2>/dev/null
     eww --config "$dir" open f3line 2>/dev/null
 ) &
 
